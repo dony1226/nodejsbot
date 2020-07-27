2b13ee0c0b983816154e40886f5b1fe4583c08c6
dony1226 <ikd1226@naver.com>
2020-07-27 22:54:58 +0900

    bot upload

---
+++ index.js
@@ -0,0 +1,35 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const token = process.env.token;
+const welcomeChannelName = "여기다";
+const byeChannelName = "여기다";
+const welcomeChannelComment = "너랑 유미랑!";
+const byeChannelComment = "다시 유미 혼자네?...";
+
+client.on('ready', () => {
+  console.log('켰다.');
+});
+
+client.on("guildMemberAdd", (member) => {
+  const guild = member.guild;
+  const newUser = member.user;
+  const welcomeChannel = guild.channels.find(channel => channel.name == welcomeChannelName);
+
+  welcomeChannel.send(`<@${newUser.id}> ${welcomeChannelComment}\n`);
+});
+
+client.on("guildMemberRemove", (member) => {
+  const guild = member.guild;
+  const deleteUser = member.user;
+  const byeChannel = guild.channels.find(channel => channel.name == byeChannelName);
+
+  byeChannel.send(`<@${deleteUser.id}> ${byeChannelComment}\n`);
+});
+
+client.on('message', (message) => {
+  if(message.content === 'ping') {
+    message.reply('pong');
+  }
+});
+
+client.login(token);
